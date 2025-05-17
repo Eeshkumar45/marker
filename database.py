29bafbf1101100c1e5a1282b9e0245289e331490
Eeshkumar45 <73717951+Eeshkumar45@users.noreply.github.com>
2025-05-17 23:29:42 +0530

    Initial commit

---
+++ database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from databases import Database
+from dotenv import load_dotenv
+load_dotenv()
+#DATABASE_URL = "postgresql://postgres:posT1234@localhost/HF"
+DATABASE_URL = os.getenv("POSTGRES")
+#DATABASE_URL = "postgresql://postgres:postgres@db:5432/mydatabase"
+
+# SQLAlchemy setup
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# databases setup (for async)
+database = Database(DATABASE_URL)
+
+Base = declarative_base()
+
+async def connect_db():
+    await database.connect()
+
+async def disconnect_db():
+    await database.disconnect()
