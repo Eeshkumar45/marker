b28fd153ccd922ac0bbf17ca9d5776f89c684849
Eeshkumar45 <73717951+Eeshkumar45@users.noreply.github.com>
2025-05-18 00:37:06 +0530

    updated cors

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://marker.wasmer.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
